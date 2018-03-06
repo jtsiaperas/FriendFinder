4c87ad84baa5f58172ffb8c82f3ccf89ee28d351
Austin Tsiaperas <liketexas@gmail.com>
2018-03-05 20:19:01 -0600

    implemented friends, getBestFriends function

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,3 +15,35 @@ function Friend(name,photo,scores) {
         return difference;
     }
 }
+
+function getBestFriend(myself, friendsList) {
+    var bestFriend = 9;
+    var closest = 100;
+    var friends = friendsList;
+
+    for (var i = 0; i<friends.length; i++)
+    {
+        var difference = myself.compare(friends[i]);
+        
+        if (difference <= closest)
+        {
+            closest = difference;
+            bestFriend = i;
+        }
+    }
+
+    return friends[bestFriend];
+}
+
+
+friendsList.push(new Friend("Jonas","N/A",[5,5,5,5,5,5,5,5]));
+
+friendsList.push(new Friend("Jose","N/A",[4,4,4,4,4,4,4,4]));
+
+friendsList.push(new Friend("Jules","N/A",[1,1,1,1,1,1,1,1]));
+
+var me = new Friend("Austin","N/A",[3,3,3,3,3,3,3,3]);
+
+var result = getBestFriend(me,friendsList);
+
+console.log(result);
