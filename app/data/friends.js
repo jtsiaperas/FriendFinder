23fd6abe744c61de5b465561b3b0b9a62ad414e4
Austin Tsiaperas <liketexas@gmail.com>
2018-03-07 02:15:37 -0600

    added modal that shows your new best friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -31,22 +31,22 @@ function getBestFriend(myself, friendsList) {
             bestFriend = i;
         }
     }
-
+    console.log(friends[bestFriend]);
     return friends[bestFriend];
 }
 
 //Testing:
 friendsList.push(new Friend("Jonas","N/A",[5,5,5,5,5,5,5,5,5,5]));
 
-friendsList.push(new Friend("Jose","N/A",[4,4,4,4,4,4,4,4]));
+// friendsList.push(new Friend("Jose","N/A",[4,4,4,4,4,4,4,4]));
 
-friendsList.push(new Friend("Jules","N/A",[1,1,1,1,1,1,1,1]));
+// friendsList.push(new Friend("Jules","N/A",[1,1,1,1,1,1,1,1]));
 
-var me = new Friend("Austin","N/A",[3,3,3,3,3,3,3,3]);
+// var me = new Friend("Austin","N/A",[3,3,3,3,3,3,3,3]);
 
-var result = getBestFriend(me,friendsList);
+// var result = getBestFriend(me,friendsList);
 
-console.log(result);
+// console.log(result);
 
 module.exports = {
     Friend, getBestFriend, friendsList
